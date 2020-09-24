9218400be6e2ae818d7c983fa771e81a1c653d01
Mikaela Fryklund <mikfry@Mikaelas-MacBook.local>
2020-09-24 21:51:39 +0200

    fix customerlist

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,54 +1,46 @@
-import React, {useState, useEffect} from 'react'
-import UserKit from '../data/UserKit'
-import {useHistory} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import UserKit from "../data/UserKit";
+import { useHistory } from "react-router-dom";
 
-export default function WelcomePage( ) {
+export default function WelcomePage() {
+  const [customerList, setCustomerList] = useState([]);
+  const userKit = new UserKit();
+  const history = useHistory();
+  const params = new URLSearchParams(history.location.search);
+  const uid = params.get("uid");
+  const token = params.get("token");
 
-   
-    const [customerList, setCustomerList] = useState([]);
-    const userKit = new UserKit();
-    const history = useHistory();
-    const params = new URLSearchParams(history.location.search);
-    const uid = params.get("uid");
-    const token = params.get("token");
-
-    
-    useEffect(() => {
-        if (token) {
-            fetchClients()
-        }
-    })
-
-    
-    function fetchClients() {
-    
-        userKit
-            .getCustomerList()
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-               
-            });
+  useEffect(() => {
+    if (token) {
+      fetchClients();
     }
-   
-   
-  
+  });
 
-    return (
-        <div>
-            
-            <h1>Welcome </h1>
+  function fetchClients() {
+    userKit
+      .getCustomerList()
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setCustomerList(data.results);
+      });
+  }
 
-            <button onClick={fetchClients}>Get customers</button>
-            {customerList.map(customerItem => {
-                return <p>{customerItem}</p>
-            })}
-            
-            <button onClick={()=> history.push("/customer-detail")}>Create new customer</button>
+  return (
+    <div>
+      <h1>Welcome </h1>
 
-            <button onClick={() => history.push("/login")}>Log out</button>
+      <button onClick={fetchClients}>Get customers</button>
+      {customerList.map((customerItem) => {
+          
+        return <p>{customerItem.name}</p>;
+      })}
 
-            
-        </div>
-    )
+      <button onClick={() => history.push("/customer-detail")}>
+        Create new customer
+      </button>
+
+      <button onClick={() => history.push("/login")}>Log out</button>
+    </div>
+  );
 }
