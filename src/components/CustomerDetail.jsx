67801a9f9a4db97dd4940476ef17c41e8403a443
Mikaela Fryklund <mikfry@Mikaelas-MacBook.local>
2020-09-28 10:24:36 +0200

    fix customerview

--- src/components/CustomerDetail.jsx
+++ src/components/CustomerDetail.jsx
@@ -1,33 +1,72 @@
 import React, { useState, useEffect } from "react";
 import UserKit from "../data/UserKit";
 import { useHistory } from "react-router-dom";
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+font-size: 1,1rem;
+color: blue;
+`
+
+
+const Text = styled.text`
+  font-size: 1rem;
+  color: blue;
+  background: whitesmoke;
+`;
+const Div = styled.div`
+  padding: 20px;
+`
+const InnerDiv = styled.div`
+widht: 55px;
+height: 75px;
+
+`
+
 
 export default function CustomerDetail() {
-  const [customer, setCustomer] = useState([]);
+  const [customerView, setCustomerView] = useState({});
   const userKit = new UserKit();
   const history = useHistory();
   const params = new URLSearchParams(history.location.search);
   const id = params.get("id");
 
   useEffect(() => {
-    fetchCustomer();
+    fetchCustomerView();
   }, []);
 
-  function fetchCustomer() {
+  function fetchCustomerView() {
     userKit
       .getCustomer(id)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        setCustomer(data.results);
+        setCustomerView(data);
       });
   }
 
   return (
-    <div>
-      <h1>
-        Test {customer.id} {customer.firstName}
-      </h1>
-    </div>
+    <Div>
+      <InnerDiv>
+        <H1>Customer {customerView.id}</H1>
+        <Text>
+          Name: {customerView.name}
+          <br /> <br />
+          OrgNr: {customerView.organisationNr}
+          <br /> <br />
+          VatNr: {customerView.vatNr}
+          <br /> <br />
+          Reference: {customerView.reference}
+          <br /> <br />
+          Payment-term: {customerView.paymentTerm}
+          <br /> <br />
+          Website: {customerView.website}
+          <br /> <br />
+          Email: {customerView.email}
+          <br /> <br />
+          Phonenumber: {customerView.phoneNumber}
+        </Text>
+      </InnerDiv>
+    </Div>
   );
 }
